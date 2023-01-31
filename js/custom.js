99d6f3f0fd58f1fe95f61353edc88870eef401dd
geekswg <geekswg@users.noreply.github.com>
2023-01-31 15:49:34 +0000

    deploy: geekswg/hugoBlog@4ece513a0bcb50acc073fc3a7044d761cc3de41f

--- js/custom.js
+++ js/custom.js
@@ -94,7 +94,7 @@ $(function() {
 
 	/* 评论框加载背景图片 */
 	//$(".v[data-class=v] .veditor").attr('style', "background-image: url("  + "/images/common/valinebg.webp) !important;");
-	$(".v[data-class=v] .veditor").attr('style',"background-color: black");
+	//$(".v[data-class=v] .veditor").attr('style',"background-color: black");
 });
 
 function getCurrentDateString() {
